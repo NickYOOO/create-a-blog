aa3c4d81bd74b298989bf3cf6406e5b2271fb840
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2023-01-14 22:17:26 +0900

    State 를 이용한 좋아요 버튼, 수정 버튼

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { useState } from 'react';
 
 function App() {
 
-  let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
-
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [ 따봉, 따봉변경 ] = useState(0);
+  
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]} <span onClick={ ()=>{ 따봉변경( 따봉 + 1 ) } } >👍</span> { 따봉 }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
