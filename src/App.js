90b65f1a90cb23f5bb97dad24290dbf15851283e
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2023-01-17 18:02:07 +0900

    State 변경함수 실습

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import { useState } from 'react';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [ 따봉, 따봉변경 ] = useState(0);
-  
+  let [따봉, 따봉변경] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy)
+      }}> 수정버튼 </button>
       <div className="list">
-        <h4>{글제목[0]} <span onClick={ ()=>{ 따봉변경( 따봉 + 1 ) } } >👍</span> { 따봉 }</h4>
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }} >👍</span> {따봉}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -24,6 +29,13 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+
     </div>
   );
 }
